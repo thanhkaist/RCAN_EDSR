d4d739eaaa2cb5fc1e02d847ad70bb86f59d34fa
Xuan Thanh <thanhkaist>
2019-10-12 21:55:37 +0900

    correct test file

--- test.py
+++ test.py
@@ -1,40 +1,17 @@
-# ------------------------------
-# EE838A
-# KAIST VIC LAB
-# 2019/09/17
-# Sehwan Ki
-# Super-Resolution
-# ------------------------------
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 from torch.autograd import Variable
 import argparse
-import numpy as np
-from torch.nn import init
-import torch.optim as optim
-import math
 from math import log10
-
 from model import model
-from data import DIV2K, Set5
+from model.model import weights_init
 from utils import *
-import time
-from scipy import io
 from PIL import Image
-from skimage.measure import compare_ssim as ssim
-from skimage.measure import compare_psnr as ssim
-import scipy
-import scipy.misc
-
+from data import get_test_dataloader
 
 parser = argparse.ArgumentParser(description='Super Resolution')
 
 # validation data
-parser.add_argument('--HR_valDataroot', required=False, default='SR_data/benchmark/Set5/HR') # modifying to your SR_data folder path
-parser.add_argument('--LR_valDataroot', required=False, default='SR_data/benchmark/Set5/LR_bicubic/X2') # modifying to your SR_data folder path
+parser.add_argument('--HR_valDataroot', required=False, default='data/benchmark/Set5/HR') # modifying to your SR_data folder path
+parser.add_argument('--LR_valDataroot', required=False, default='data/benchmark/Set5/LR_bicubic/X2') # modifying to your SR_data folder path
 parser.add_argument('--valBatchSize', type=int, default=5)
 
 parser.add_argument('--pretrained_model', default='result/Net1/model/model_lastest.pt', help='save result')
@@ -60,47 +37,19 @@ elif args.gpu == 1:
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv2d') != -1:
-        init.xavier_normal_(m.weight.data)
 
-
-def get_testdataset(args):
-    data_test = Set5(args)
-    dataloader = torch.utils.data.DataLoader(data_test, batch_size=1,
-                                             drop_last=True, shuffle=False, num_workers=int(args.nThreads), pin_memory=True)
-    return dataloader
-
-
-def ssim_from_sci(img1, img2, padding=4):
-    img1 = Image.fromarray(np.uint8(img1) ,mode='RGB')
-    img1 = img1.convert('YCbCr')
-    img1 = np.ndarray((img1.size[1], img1.size[0], 3), 'u1', img1.tobytes())
-    img2 = Image.fromarray(np.uint8(img2) ,mode='RGB')
-    img2 = img2.convert('YCbCr')
-    img2 = np.ndarray((img2.size[1], img2.size[0], 3), 'u1', img2.tobytes())
-    # get channel Y
-    img1 = img1[:, :, 0]
-    img2 = img2[:, :, 0]
-    # padding
-    # import pdb; pdb.set_trace()
-    img1 = img1[padding: -padding, padding:-padding]
-    img2 = img2[padding: -padding, padding:-padding]
-    ss = ssim(img1, img2)
-    return ss 
 
 
 def test(args):
 
     # SR network
-    my_model = model.Net1(args)
+    my_model = model.EDSR()
     my_model.apply(weights_init)
     my_model.cuda()
 
     my_model.load_state_dict(torch.load(args.pretrained_model))
 
-    testdataloader = get_testdataset(args)
+    testdataloader = get_test_dataloader('Set5',args)
     my_model.eval()
 
     avg_psnr = 0
@@ -113,46 +62,19 @@ def test(args):
             im_hr = Variable(im_hr.cuda())
             output = my_model(im_lr)
 
-        output = output.cpu()
-        output = output.data.squeeze(0)
-        mean = [0.5, 0.5, 0.5]
-        std = [0.5, 0.5, 0.5]
-        for t, m, s in zip(output, mean, std):
-            t.mul_(s).add_(m)
-
-        output = output.numpy()
-        output *= 255.0
-        output = output.clip(0, 255)
-        # import pdb; pdb.set_trace()
-        # Convert to W*H*3 and BGR to RGB
-        out = Image.fromarray(np.uint8(output.transpose([1,2,0])[...,::-1]),mode='RGB')  # output of SRCNN
+        output = unnormalize(output)
+        out = Image.fromarray(np.uint8(output),mode='RGB')  # output of SRCNN
         out.save('result/Net1/SR_img_%03d.png' % (count))
 
         # =========== Target Image ===============
-        im_hr = im_hr.cpu()
-        im_hr = im_hr.data.squeeze(0)
-        mean = [0.5, 0.5, 0.5]
-        std = [0.5, 0.5, 0.5]
-        for t, m, s in zip(im_hr, mean, std):
-            t.mul_(s).add_(m)
+        im_hr = unnormalize(im_hr)
 
-        im_hr = im_hr.numpy()
-        im_hr *= 255.0
-        im_hr = im_hr.clip(0, 255)
-
-
-        mse = ((im_hr[:, 8:-8,8:-8] - output[:, 8:-8,8:-8]) ** 2).mean()
-        psnr = 10 * log10(255 * 255 / (mse + 10 ** (-10)))
-        psnr_val = psnr
-
-        # calculate multichannel ssimd
-        # ssim_val = ssim(im_hr[:, 8:-8,8:-8].transpose(1,2,0),output[:, 8:-8,8:-8].transpose([1,2,0]),multichannel=True)
-        ssim_val = ssim_from_sci(im_hr[:, 8:-8,8:-8].transpose(1,2,0)[...,::-1],output[:, 8:-8,8:-8].transpose([1,2,0])[...,::-1])
-        print( '%d_img PSNR/SSIM: %.4f/%.4f '%(count,psnr_val,ssim_val))
-        avg_ssim += ssim_val
+        psnr , ssim = psnr_ssim_from_sci(output,im_hr)
+        print( '%d_img PSNR/SSIM: %.4f/%.4f '%(count,psnr,ssim))
+        avg_ssim += ssim
         avg_psnr += psnr
 
-    print('AVG PSNR/AVG SSIM : %.4f/%.4f '%(avg_psnr/5,avg_ssim / 5))
+    print('AVG PSNR/AVG SSIM : %.4f/%.4f '%(avg_psnr/len(testdataloader.dataset),avg_ssim / len(testdataloader.dataset)))
 
 
 if __name__ == '__main__':
